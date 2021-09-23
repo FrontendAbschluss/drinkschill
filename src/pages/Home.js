e4076db468ca51b62f36f8725bb22f5699607e10
Jonas Ermert <ermert.jonas@yahoo.de>
2021-09-23 11:04:24 +0200

    Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,4 +6,15 @@ const Home = () => {
     );
 }
 
+
+{data.map((e, i) => <div className={`
+            style${Math.floor((i % 6) + 1)}
+            ${i % 2 == 0 ? 'left' : 'right'}
+            `} 
+            key={e.id}
+            >
+            {e.name}
+            </div>)}
+
+
 export default Home;
